fb33eec66351a7efb9e5d7b3d62bfdd850790f42
Cohe-rent <akshram.pandiyan@coherent.in>
2025-02-27 14:42:16 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,8 +1,4 @@
-// vars/helloWorld.groovy
-def call() {
-    echo "Hello from Shared Library"
-}
-
-def sayHello(String name) {
-    echo "Hello, ${name}!"
-}
+jenkins-shared-library/
+├── vars/
+│   ├── sayHello.groovy
+│   ├── otherFunctions.groovy
